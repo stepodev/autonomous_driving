4c78e4c302a2a1bd31f37d5c9dfd4fb5e1a8e5dc
tylernewnoise <falko.becker@informatik.hu-berlin.de>
2017-11-20 12:53:55 +0100

    removed dist-upgrade

--- setup_ros1.sh
+++ setup_ros1.sh
@@ -12,9 +12,6 @@ sudo apt-get update
 sleep 3
 sudo apt-get upgrade
 sleep 3
-sudo apt-get dist-upgrade
-sleep 3
-
 
 #Setup your sources.list
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
