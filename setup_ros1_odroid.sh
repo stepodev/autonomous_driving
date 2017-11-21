9308b58e89b8062e107a67ebcfb9acfeb84f5cd3
Benjamin <benjamin.lorenz@poseto.de>
2017-11-21 16:26:51 +0100

    install script is only for odroids

---
+++ setup_ros1_odroid.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo -e "\n\t\tUnd ab geht die wilde Fahrt!\n"
+
+sleep 2
+
+
+sudo apt-get update 
+sleep 3
+sudo apt-get upgrade
+sleep 3
+
+#Setup your sources.list
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+
+#Set up your keys
+sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+set +e
+sudo apt-get update
+set -e
+sudo apt-get install ros-lunar-desktop-full
+
+sudo rosdep init
+rosdep update
+
+#Dependencies for building packages
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential
+
+#environment variables
+echo "source /opt/ros/lunar/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+set +x
+echo -e "\t...läuft...\n"
+
+sleep 5
+
+echo -e "\t...jetzt...\n"
+
+sleep 3
+
+echo -e "\n\n\n\n\t\t\t\t\t\t."
+
+
