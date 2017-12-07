dd018f4c0451918a20480b6e9b3bb60bebd9623e
Benjamin <benjamin.lorenz@poseto.de>
2017-12-07 13:27:58 +0100

    first_setup.sh checks if turtlesim is installed

--- catkin_ws/first_setup.sh
+++ catkin_ws/first_setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if rospack find turtlesim;then
+    echo 'Go go go!'
+else
+    rosdep install turtlesim &&
+    echo "turtlesim installed"
+fi
+
 catkin_make &&
 source devel/setup.bash &&
 source ~/.bashrc &&
