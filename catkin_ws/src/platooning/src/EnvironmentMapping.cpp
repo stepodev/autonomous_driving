e01a9b58bbd49ea0c054a5dd97f4dbddf10841df
heydenjq <jonas.heyden@informatik.hu-berlin.de>
2017-12-17 15:18:07 +0100

    trying to get the nodelet publish something

--- catkin_ws/src/platooning/src/EnvironmentMapping.cpp
+++ catkin_ws/src/platooning/src/EnvironmentMapping.cpp
@@ -1,12 +1,18 @@
 #include <pluginlib/class_list_macros.h>
 #include "EnvironmentMapping.h"
-
+#include <std_msgs/Float64.h>
 
 namespace platooning
 {
     void EnvironmentMapping::onInit()
     {
-        NODELET_DEBUG("Initializing nodelet...");
+        nh = getNodeHandle();
+        pub = nh.advertise<std_msgs::Float64>("distance_to_obj",1000);
+        // output from optical capturing
+        std_msgs::Float64Ptr output(new std_msgs::Float64());
+        output->data = 0.33;
+        pub.publish(output);
+        NODELET_DEBUG("the distance is %f", output->data);
     }
 }
 
