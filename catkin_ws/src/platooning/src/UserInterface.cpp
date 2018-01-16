b88e2044af0589372973856a3c6b839a9a3d9b7e
Falko <falko.becker@informatik.hu-berlin.de>
2018-01-16 19:42:46 +0100

    Ich habe Leben erschaffen! 1st draw for UI, looks ugly, like all of this. Works. TODO: launchfile

--- catkin_ws/src/platooning/src/UserInterface.cpp
+++ catkin_ws/src/platooning/src/UserInterface.cpp
@@ -1,88 +1,24 @@
-//
-// Created by stepo on 12/16/17.
-//
-
-
-/**
- * @file /platooning/src/platooning.cpp
- *
- * @brief Nodelet implementation of RemoteContol
- *
- * @author stepo
- **/
-
-/*****************************************************************************
-** Includes
-*****************************************************************************/
-// %Tag(FULLTEXT)%
 #include "UserInterface.hpp"
 
-namespace platooning {
+namespace platooning
+{
+	UserInterface::UserInterface() {};
+	UserInterface::~UserInterface() {};
 
+	void UserInterface::onInit() {
+		userInterfaceSubsc = nh_.subscribe("UserInterface", 1000, &UserInterface::msg_callback, this);
+	};
 
-/*****************************************************************************
-** Constructors
-*****************************************************************************/
-
-
-/**
- * @brief Template Nodelet
- */
-
-  UserInterface::UserInterface() {};
-
-
-
-/*****************************************************************************
-** Destructors
-*****************************************************************************/
-
-  UserInterface::~UserInterface() {};
-
-
-/*****************************************************************************
-** Initializers
-*****************************************************************************/
-
-  /**
-  * Set-up necessary publishers/subscribers
-  * @return true, if successful
-  */
-  void UserInterface::onInit() {
-
-    //subscribers of protocol nodelet
-    templateSubscriber = nh_.subscribe("templateMsg", 10,
-                                                  &UserInterface::templateTopicHandler, this);
-
-    //publisher of forced driving vector
-    templatePublisher = nh_.advertise< platooning::templateMsg >("commands/templateMsg", 10);
-
-
-  };
-
-
-/*****************************************************************************
-** Handlers
-*****************************************************************************/
-
-  /*
-   * handling an event and publishing something
-   */
-  void UserInterface::templateTopicHandler(const platooning::templateMsg msg) {
-
-    NODELET_DEBUG("handling a template");
-
-    if( msg.templatebool || !msg.templatebool ) {
-      templatePublisher.publish(msg);
-    } else {
-      NODELET_WARN("warning you of stuff");
-    }
-
-  }
-
-
-
-} // namespace platooning
+	void UserInterface::msg_callback(const platooning::userInterface& subscMsg)
+	{
+		std::string lv = "Yes";
+		std::string fv = "Yes";
+		if (subscMsg.leading_vehicle == 0)
+			lv = "No";
+		if (subscMsg.following_vehicle == 0)
+			fv = "No";
+		std::cout << "\r" << "LV: " << lv << " | FV: " << fv << " | IPD: " << subscMsg.inner_platoon_distance << " | PSpeed: " << subscMsg.platoon_speed << " | VSpeed: " << subscMsg.speed << " " << std::flush;
+	}
+}
 
 PLUGINLIB_EXPORT_CLASS(platooning::UserInterface, nodelet::Nodelet);
-// %EndTag(FULLTEXT)%
