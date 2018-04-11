d8069c87ebfac568bcb921feaf1e5c059d741422
root <root@thinker.lan>
2018-04-11 22:39:20 +0200

    eth to odroid

--- scripts/adhoc.sh
+++ scripts/adhoc.sh
@@ -1,8 +1,8 @@
 service network-manager stop
-ip link set wlan0 down
-iwconfig wlan0 mode ad-hoc
-iwconfig wlan0 channel 4
-iwconfig wlan0 essid SPHAF
-ip link set wlan0 up
-ip addr add 10.42.43.100 dev wlan0
-ifconfig wlan0
+ip link set wlp3s0 down
+iwconfig wlp3s0 mode ad-hoc
+iwconfig wlp3s0 channel 4
+iwconfig wlp3s0 essid SPHAF
+ip link set wlp3s0 up
+ip addr add 10.42.43.101 dev wlp3s0
+ifconfig wlp3s0
